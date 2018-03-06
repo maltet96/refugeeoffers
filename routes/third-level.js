2c273881a40b697e364869edaf6fc4d3c95de3c6
Malte Tölle <malte.toelle@gmail.com>
2018-03-06 22:59:09 +0100

    Coverpicture is now shown on all levels & description on third & removed border around descriptions

--- routes/third-level.js
+++ routes/third-level.js
@@ -12,7 +12,7 @@ const client = contentful.createClient({
 })
 
 router.get('/:firstCategory/:secondCategory', function(req, res, next) {
-    
+
   // get all first level categories
   client.getEntries({
     'locale': req.query["lang"],
@@ -28,7 +28,7 @@ router.get('/:firstCategory/:secondCategory', function(req, res, next) {
     let entriesFilteredForCategories = entries.items.filter(function(entry){
       return entry.sys.contentType.sys.id == 'offering'
     })
-    
+
     let allOfferings = entriesFilteredForCategories.map((offering) => {
 
       try{
@@ -38,6 +38,7 @@ router.get('/:firstCategory/:secondCategory', function(req, res, next) {
           secondCategories: offering.fields.nd2ndCategory,
           institution: offering.fields.institution,
           picture: offering.fields.picture ? offering.fields.picture.fields.file.url : offering.fields.picture,
+          description: (offering.fields.description)? offering.fields.description.split(/\n|\s\n/).join("<br>\n") + "<br>" : null,
           openingHours: offering.fields.openingHours ?moffering.fields.openingHours.replace(";", "<br>") : null,
           contactPersonPhoneNumber: offering.fields.contactPersonPhoneNumber,
           contactPersonEmailAddress: offering.fields.contactPersonEmailAddress,
@@ -60,7 +61,7 @@ router.get('/:firstCategory/:secondCategory', function(req, res, next) {
         if (offering.secondCategories){
 
           let validSecondCategories = offering.secondCategories.map((offering) => {return offering.sys.id});
-  
+
           return validSecondCategories.includes(req.params["secondCategory"]);
           }
       }
@@ -79,8 +80,8 @@ router.get('/:firstCategory/:secondCategory', function(req, res, next) {
 
     // filter the languages out of all entries fetched
     let languages = entriesFilteredForLanguages.map((language) => {
-      
-      // make de and en appear first 
+
+      // make de and en appear first
       if(language.fields.shortForm == "de"){
         language.order = 1;
 
@@ -91,7 +92,7 @@ router.get('/:firstCategory/:secondCategory', function(req, res, next) {
       else {
         language.order = 3
       }
-    
+
       // for some reason Contentful fucks with Somali and delivers the full name as the language code, so we are changing it manually now
       if(language.fields.languageCode == "Somali"){
         language.fields.languageCode = "so"
@@ -109,13 +110,14 @@ router.get('/:firstCategory/:secondCategory', function(req, res, next) {
      entriesFilteredForFrontPage = entries.items.filter(function(entry){
       return entry.sys.contentType.sys.id == 'frontPage'
     })
-  
+
     let frontPage = entriesFilteredForFrontPage.map((element) => {
       return {
         title: element.fields.title,
-        description: element.fields.description
+        description: element.fields.description,
+        coverpicture: element.fields.coverpicture.fields.file.url
       }
-      })[0]   
+    })[0]
 
     // filter only imprint out of all entries fetched
     entriesFilteredForImprint = entries.items.filter(function(entry){
