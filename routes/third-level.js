00d04888ce6748aae56211c692cf4b68ecd409f5
Malte Tölle <malte.toelle@gmail.com>
2018-03-14 12:17:17 +0100

    CSS: adapt Doro's design on third level - pic, mailAndPhone, openingHoursAndAdresse next to each other with line seperating them - width and font size adjustments
    thirdLevel: get pic from c
    
    third: change order of offering specs
    
    first-level: add another <br> between two passages in frontPage

--- routes/third-level.js
+++ routes/third-level.js
@@ -36,9 +36,9 @@ router.get('/:firstCategory/:secondCategory', function(req, res, next) {
           name: offering.fields.title,
           firstCategories: offering.fields.nd1stCategory,
           secondCategories: offering.fields.nd2ndCategory,
-          institution: offering.fields.institution,
-          picture: offering.fields.picture ? offering.fields.picture.fields.file.url : offering.fields.picture,
+          /*institution: offering.fields.institution,*/
           description: (offering.fields.description)? offering.fields.description.split(/\n|\s\n/).join("<br>\n") + "<br>" : null,
+          picture: offering.fields.picture ? offering.fields.picture.fields.file.url : offering.fields.picture,
           openingHours: offering.fields.openingHours ?moffering.fields.openingHours.replace(";", "<br>") : null,
           contactPersonPhoneNumber: offering.fields.contactPersonPhoneNumber,
           contactPersonEmailAddress: offering.fields.contactPersonEmailAddress,
@@ -145,5 +145,9 @@ router.get('/:firstCategory/:secondCategory', function(req, res, next) {
     console.log(err);
   })
 });
+
+/*client.getContentTypes()
+.then((response) => console.log(response.items))
+.catch(console.error)*/
 
 module.exports = router;
